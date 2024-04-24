598ebf6ed8e092c1d86a129f5528696a7d9f980d
JackObid <jacqueobi@gmail.com>
2024-04-24 15:41:30 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Your respond in a short one and its conversational friendly style!'));
+app.get('/', (req, res) => res.send('The zebra leading the lion on a wild chase through the tall grass!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
